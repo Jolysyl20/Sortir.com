0788e81aab88d1fa1ebfda3aff6dcc6108d9d230
Jolysyl20 <72710893+Jolysyl20@users.noreply.github.com>
2021-10-18 00:57:40 +0200

    Maquette

--- BONUS SQL/baseVille.sql
+++
@@ -1,6 +0,0 @@
-insert into ville (nom_ville,code_postal) values
-("Niort","79000"),
-("Bessine","79000"),
-("Nantes","44000"),
-("rennes","35000"),
-
--- BONUS SQL/participant.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO participant (pseudo, roles, password, nom, prenom, mail, actif, site_participant_id) VALUES  
-('Enrick', ["ROLE_ADMIN"],'azerty', 'ENET', 'Enrick', 'enrick.enet2020@campus-eni.fr',1, 3 ), 
