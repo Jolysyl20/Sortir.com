1f1c41b934f197b856e32765349bc50bba149d28
Jolysyl20 <72710893+Jolysyl20@users.noreply.github.com>
2021-10-18 09:26:37 +0200

    Update participant.sql

--- BONUS SQL/participant.sql
+++ BONUS SQL/participant.sql
@@ -1,2 +1,2 @@
 INSERT INTO participant (pseudo, roles, password, nom, prenom, mail, actif, site_participant_id) VALUES  
-('Enrick', ["ROLE_ADMIN"],'azerty', 'ENET', 'Enrick', 'enrick.enet2020@campus-eni.fr',1, 3 ), 
+('Syl20', ["ROLE_ADMIN"],'azerty79', 'JOLY', 'Sylvain', 'sylvain.joly2020@campus-eni.fr',1, 3 ), 
